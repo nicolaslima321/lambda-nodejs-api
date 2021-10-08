ede1a00fc1d5fd118a6f0fd6c25da12f6136e1ef
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2021-10-07 22:00:26 -0300

    extracted and refactor Offer to OfferService

--- api/offer.js
+++ api/offer.js
@@ -1,101 +1,40 @@
-const uuid = require('uuid');
-const AWS = require('aws-sdk');
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
+const __MODULE__ = 'Offer';
 const { mountedResponse } = require('./utils/response');
-const { DEFAULT_UUID, LOCATION_TABLE, OFFER_TABLE } = process.env
+const offerService = require('./services/offerService');
 
 module.exports.create = async (event) => {
   const requestBody = JSON.parse(event.body);
   const { ...offerParams } = requestBody;
 
   if (!offerParams.name || !offerParams.brandId) {
-    console.error('Bad request');
+    console.log(`${__MODULE__}@create: Invalid or missing params`, event);
     const body = { message: 'Could not create offer, invalid or missing params' };
 
     return mountedResponse(body, 400);
   }
 
-  const currentTimestamp = new Date().now;
+  const offerCreated = offerService.create(offerParams);
 
-  const offer = {
-    id: uuid.v1(),
-    publisherId: offerParams.publisherId || DEFAULT_UUID,
-    name: offerParams.name,
-    brandId: offerParams.brandId,
-    locationsTotal: 0,
-    startDate: offerParams.startDate || currentTimestamp,
-    created: currentTimestamp,
-    updated: currentTimestamp,
-  };
-
-  const offerInfo = {
-    TableName: OFFER_TABLE,
-    Item: offer,
-  };
-
-  try {
-    await dynamoDb.put(offerInfo).promise();
-    const body = {
-      message: 'Sucessfully created this offer',
-      offerId: offer.id
-    };
-
-    return mountedResponse(body, 201);
-
-  } catch (error) {
-
+  if (!offerCreated) {
     const body = { message: 'Could not create this offer' };
     return mountedResponse(body, 500);
   }
+
+  const body = { message: 'Offer successfully created!', offer: offerCreated };
+  return mountedResponse(body, 200);
 };
 
 module.exports.linkToLocation = async (event) => {
   const { offerId, locationId } = event.pathParameters;
 
-  // TODO: Refactor all Offer & Location operations to an service
-  const offerParams = {
-    TableName: OFFER_TABLE,
-    Key: { id: offerId }
-  }
+  // TODO: get location and offer and send as parameters
+  const offerWasLinked = offerService.linkToLocation(offerId, locationId);
 
-  const locationParams = {
-    TableName: LOCATION_TABLE,
-    Key: { id: locationId }
-  }
-
-  try {
-    offer = await dynamoDb.get(offerParams).promise();
-    location = await dynamoDb.get(locationParams).promise();
-
-    const locationParams = {
-      TableName: LOCATION_TABLE,
-      Key: { locationId },
-      UpdateExpression: "set hasOffer = :h",
-      ExpressionAttributeValues: { ":h": true },
-      ReturnValues: "UPDATED_NEW"
-    };
-
-    const offerParams = {
-      TableName: OFFER_TABLE,
-      Key: { offerId },
-      UpdateExpression: "set locationsTotal = :l",
-      ExpressionAttributeValues: { ":l": offer.locationsTotal + 1 },
-      ReturnValues: "UPDATED_NEW"
-    };
-
-    await dynamoDb.update(offerParams).promise();
-    await dynamoDb.update(locationParams).promise();
-
-    const body = { message: 'Offer successfully linked to this Location' };
-
-    return mountedResponse(body, 200);
-
-  } catch (error) {
-    console.log(error);
+  if (!offerWasLinked) {
     const body = { message: 'Could not link this Offer to this Location' };
-
     return mountedResponse(body, 500);
   }
+
+    const body = { message: 'Offer successfully linked to this Location' };
+    return mountedResponse(body, 200);
 };
---
+++ api/services/offerService.js
@@ -0,0 +1,80 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const __MODULE__ = 'OfferService';
+const { DEFAULT_UUID, LOCATION_TABLE, OFFER_TABLE } = process.env
+
+module.exports.create = async (offerParams) => {
+  const currentTimestamp = new Date().now;
+
+  const offer = {
+    id: uuid.v1(),
+    publisherId: offerParams.publisherId || DEFAULT_UUID,
+    name: offerParams.name,
+    brandId: offerParams.brandId,
+    locationsTotal: 0,
+    startDate: offerParams.startDate || currentTimestamp,
+    created: currentTimestamp,
+    updated: currentTimestamp,
+  };
+
+  const offerInfo = { TableName: OFFER_TABLE, Item: offer };
+
+  try {
+    await dynamoDb.put(offerInfo).promise();
+
+    return offer;
+
+  } catch (error) {
+    console.log(`${__MODULE__}@create: An error ocurred for offer creation`, error);
+
+    return false;
+  }
+};
+
+module.exports.getById = async (offerId) => {
+  const offerParams = {
+    TableName: OFFER_TABLE,
+    Key: { id: offerId }
+  }
+
+  try {
+    return await dynamoDb.get(offerParams).promise();
+
+  } catch (error) {
+    console.log(`${__MODULE__}@getById: An error ocurred to get offer #${offerId}`, error);
+
+    return null;
+  }
+};
+
+module.exports.linkToLocation = async (offer, location) => {
+  const locationParams = {
+    TableName: LOCATION_TABLE,
+    Key: { id: location.id },
+    UpdateExpression: "set hasOffer = :h",
+    ExpressionAttributeValues: { ":h": true },
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  const offerParams = {
+    TableName: OFFER_TABLE,
+    Key: { id: offer.id },
+    UpdateExpression: "set locationsTotal = :l",
+    ExpressionAttributeValues: { ":l": offer.locationsTotal + 1 },
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  try {
+    await dynamoDb.update(offerParams).promise();
+    await dynamoDb.update(locationParams).promise();
+
+    return true;
+  } catch (error) {
+    console.log(`${__MODULE__}@linkToLocation: An error ocurred to link offer #${offer.id} to location #${location.id}`, error);
+
+    return false;
+  }
+};
